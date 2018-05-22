0aeb5b71d7cec1f27fd308964359be8b2affd448
Matt Zenel <matt.zenel@august.com>
2018-05-21 19:26:14 -0700

    add jenkins remote build trigger

---
+++ jenkins_remote_build_trigger.py
@@ -0,0 +1,40 @@
+"""This module triggers a remote jenkins job"""
+
+import os
+import requests
+
+def build_trigger():
+    '''remote jenkins job post request'''
+    jenkins_user = os.environ['JENKINS_USER']
+    jenkins_pw = os.environ['JENKINS_PW']
+    jenkins_host = os.environ['JENKINS_HOST']
+    jenkins_job = os.environ['JENKINS_JOB']
+
+    jenkins = [
+        "http://",
+        jenkins_user,
+        ":",
+        jenkins_pw,
+        "@",
+        jenkins_host,
+        "/jenkins/job/",
+        jenkins_job,
+        "/build"
+    ]
+
+    jenkins_trigger = ''.join(jenkins)
+
+    try:
+        response = requests.post(jenkins_trigger)
+    except requests.ConnectionError:
+        print ("Error:", response)
+    except requests.HTTPError:
+        print("Error:", response)
+
+    if response.status_code != 201:
+        print("Error: status_code:", response.status_code)
+        print("Full response:", response)
+    else:
+        print("Build triggered successfully:", response)
+
+build_trigger()
